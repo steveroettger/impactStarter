1c5d8fe9bc528337ec424a30cf2473f648f28d3c
steveroettger <steve@steveroettger.com>
2013-09-30 09:05:44 -0400

    fixing project status boolean in migration file

--- db/migrate/20130612152508_change_status_type_to_projects.rb
+++ db/migrate/20130612152508_change_status_type_to_projects.rb
@@ -1,7 +1,7 @@
 class ChangeStatusTypeToProjects < ActiveRecord::Migration
   def change
     change_table(:projects) do |t|
-      t.change :status, :boolean, default: false
+      t.change :status, :boolean
     end
   end
 end
