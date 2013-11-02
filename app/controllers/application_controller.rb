930d461413cf86c7361e6f24467a90a116a7ae8c
Andrew Briening <abriening@gmail.com>
2013-11-02 10:44:55 -0400

    This _should_ handle LinkedIn throttling and authorization errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,10 +7,20 @@ class ApplicationController < ActionController::Base
   include ErrorResponseActions
   # rescue_from CanCan::AccessDenied, :with => :authorization_error
   rescue_from ActiveRecord::RecordNotFound, :with => :resource_not_found
+  rescue_from LinkedIn::Errors::AccessDeniedError, :with => :linkedin_error
 
   private
 
   def authenticate!
     current_user || devise_controller? || ( authentication_error and return false )
+  end
+
+  def linkedin_error
+    # 403 Forbidden - LinkedIn Error
+    respond_to do |format|
+      format.html{ render '/rescues/linkedin_error', :status => 403 }
+      format.xml{  render :xml => 'LinkedIn Error',  :status => 403 }
+      format.json{ render :json => 'LinkedIn Error', :status => 403 }
+    end
   end
 end
